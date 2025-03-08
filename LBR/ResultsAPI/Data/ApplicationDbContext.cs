d904eefa360dc405a07059bc373fd04a347dfa2a
kylejhargreaves <102608293+kylejhargreaves@users.noreply.github.com>
2025-03-08 12:19:43 +0000

    Rejig skeleton

---
+++ LBR/ResultsAPI/Data/ApplicationDbContext.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.EntityFrameworkCore;
+using ResultsAPI.Models;
+
+namespace ResultsAPI.Data
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<Player> Players { get; set; }
+    }
+}
---
+++ LBR/ResultsAPI/Models/Player.cs
@@ -0,0 +1,9 @@
+﻿namespace ResultsAPI.Models
+{
+    public class Player
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public int Age { get; set; }
+    }
+}
